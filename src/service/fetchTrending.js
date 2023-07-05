f4356336d1436dcd2126663d2e16da9a92bfc485
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-07-05 10:04:11 +0200

    .

---
+++ src/service/fetchTrending.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTljNmRlOWVmN2U5OWJmMWJiYmE0NzFjZTRkM2YyMyIsInN1YiI6IjY0YTUxZjgwZGExMGYwMDExY2I0NzlkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P3RpoF7N4clR-vZQeo4ofCPwjtK1obP9lrkTduiwnF0',
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
