f4b6083e23474fa1a6ee2128d0e67365914fa5f4
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-07-06 15:00:45 +0200

    .

--- src/service/fetchTrending.js
+++ src/service/fetchTrending.js
@@ -1,8 +1,12 @@
-const axios = require('axios');
+import axios from 'axios';
+
+const api_url = 'https://api.themoviedb.org/3/trending/movie/day';
+
+const api_key = `7e9c6de9ef7e99bf1bbba471ce4d3f23`;
 
 const options = {
   method: 'GET',
-  url: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+  url: 'https://api.themoviedb.org/3/trending/all/day?language=en-US',
   headers: {
     accept: 'application/json',
     Authorization:
@@ -10,11 +14,32 @@ const options = {
   },
 };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
+/* export const fetchTrending = async () => {
+  //console.log(`${api_url}?api_key=${api_key}`);
+  const { response } = await axios.get(`${api_url}?api_key=${api_key}`);
+
+  return response;
+}; */
+
+export const fetchTrending = async () => {
+  const response = await axios.request(options);
+  return response;
+};
+
+/* async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
     console.error(error);
-  });
+  }
+} */
+
+/* axios
+.request(options)
+.then(function (response) {
+  console.log(response.data);
+})
+.catch(function (error) {
+  console.error(error);
+}); */
